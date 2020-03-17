c97bc8247aa67e3f072155aa0c203f5a83a46494
Emre Sert <09370754@ksnet.local>
2020-03-17 15:17:19 +0300

    BookContext is created

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from '../src/components/root/App';
 import * as serviceWorker from './serviceWorker';
+import BookContextProvider from './contexts/BookContext';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<BookContextProvider>
+    <App />
+</BookContextProvider>
+
+
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
